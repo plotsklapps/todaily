5f16610ed7990bc81b26350fd7acf59fae70cc5a
plotsklapps <plotsklapps@gmail.com>
2025-05-07 01:48:17 +0200

    - Changed to ImagePickerGrid.

--- lib/screens/today_screen.dart
+++ lib/screens/today_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:signals/signals_flutter.dart';
 import 'package:todaily/now_signal.dart';
 import 'package:todaily/state/image_signal.dart';
 
@@ -63,10 +61,9 @@ class _TodayScreenState extends State<TodayScreen> {
               },
             ),
             const SizedBox(height: 8),
-            const Text("today's image"),
-            ImagePreview(image: _pickImage.image.watch(context)),
+            const Text("today's images"),
+            const ImagePickerGrid(),
             const SizedBox(height: 16),
-            ImagePickerButtons(onPickImage: _pickImage.pickImage),
             const SizedBox(height: 16),
           ],
         ),
@@ -189,78 +186,59 @@ class MoodSelection extends StatelessWidget {
   }
 }
 
-class ImagePreview extends StatelessWidget {
-  const ImagePreview({this.image, super.key});
-  final Uint8List? image;
+class ImagePickerGrid extends StatefulWidget {
+  const ImagePickerGrid({super.key});
+
+  @override
+  State<ImagePickerGrid> createState() => _ImagePickerGridState();
+}
+
+class _ImagePickerGridState extends State<ImagePickerGrid> {
+  final List<Uint8List?> _images = List<Uint8List?>.filled(4, null);
+
+  void _pickImage(int index) async {
+    // TODO: Implement image picking logic here
+    // For now, simulate picking an image by assigning a placeholder Uint8List
+    final Uint8List placeholderImage = Uint8List(
+      0,
+    ); // Replace with actual image data
+    setState(() {
+      _images[index] = placeholderImage;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (image == null) return const SizedBox.shrink();
-
-    return FutureBuilder<ImageInfo>(
-      future: _getImageInfo(image!),
-      builder: (BuildContext context, AsyncSnapshot<ImageInfo> snapshot) {
-        if (!snapshot.hasData) {
-          return const CircularProgressIndicator();
-        }
-
-        final ImageInfo imageInfo = snapshot.data!;
-        final bool isPortrait = imageInfo.image.height > imageInfo.image.width;
-
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxHeight: isPortrait ? 300 : 160,
-              maxWidth: isPortrait ? 160 : 300,
-            ),
-            child: ColoredBox(
-              color: Colors.grey,
-              child: Image.memory(image!, fit: BoxFit.contain),
-            ),
+    return GridView.builder(
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+      ),
+      itemCount: 4,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            if (_images.where((image) => image != null).length < 4) {
+              _pickImage(index);
+            }
+          },
+          child: Card(
+            elevation: 4,
+            child:
+                _images[index] != null
+                    ? Image.memory(_images[index]!, fit: BoxFit.cover)
+                    : const Center(
+                      child: FaIcon(
+                        FontAwesomeIcons.plus,
+                        size: 40,
+                        color: Colors.grey,
+                      ),
+                    ),
           ),
         );
       },
-    );
-  }
-
-  Future<ImageInfo> _getImageInfo(Uint8List imageData) async {
-    final Completer<ImageInfo> completer = Completer<ImageInfo>();
-    final Image image = Image.memory(imageData);
-    image.image
-        .resolve(ImageConfiguration.empty)
-        .addListener(
-          ImageStreamListener((ImageInfo info, _) {
-            completer.complete(info);
-          }),
-        );
-    return completer.future;
-  }
-}
-
-class ImagePickerButtons extends StatelessWidget {
-  const ImagePickerButtons({required this.onPickImage, super.key});
-  final VoidCallback onPickImage;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Expanded(
-          child: ElevatedButton(
-            onPressed: onPickImage,
-            child: const FaIcon(FontAwesomeIcons.fileImage),
-          ),
-        ),
-        const SizedBox(width: 8),
-        Expanded(
-          child: ElevatedButton(
-            onPressed: () {},
-            child: const FaIcon(FontAwesomeIcons.camera),
-          ),
-        ),
-      ],
     );
   }
 }
